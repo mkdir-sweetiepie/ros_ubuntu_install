b33bc4e9f2d32395050779fc415704030d7bc0fb
Hong Jihyeon <abab4551817@kw.ac.kr>
2024-08-26 05:04:57 +0200

    Update ros2_humble_install.sh

--- ros2_humble_install.sh
+++ ros2_humble_install.sh
@@ -121,7 +121,8 @@ case $ubuntu in
       custom_echo "echo ros resources" "green"
       sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg  # ROS 키를 추가
       custom_echo "adding key" "green"
-      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/nullcustom_echo "updating" "green"
+      echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+      custom_echo "updating" "green"
       sudo apt update  # 패키지 목록 업데이트
       custom_echo "installing ROS-humble-desktop-full" "green"
       sudo apt install -y ros-humble-desktop-full  # ROS Humble 설치
